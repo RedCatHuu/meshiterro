7874faa1bc6ac0eadbb310acc61e5bde329957df
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-08 08:01:53 +0000

    [add]サインアップ、ログイン、ログアウト機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  #devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+  #configure_permitted_parametersメソッドが実行されます。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  
+  def configure_permitted_parameters
+    #このメソッドは、devise_parameter_sanitizer.permitメソッドを使うことで
+    #ユーザー登録(sign_up)の際に、ユーザー名(name)の
+    #データ操作を許可しています。
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
