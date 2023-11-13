b222f518aa1b9b0a2f551aef082e24461e5644bd
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-13 05:44:33 +0000

    [add]bootstrap追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     # @userに関連付けられた投稿すべて(.post_images)を取得し代入
     # 結果的に、全体の投稿ではなく、個人が投稿したものすべてを表示できる。
     # @post_image.userの対 => @post_imageに紐づいたユーザー情報の取得
-    @post_images = @user.post_images
+    # @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -21,6 +24,13 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end 
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end 
   end 
   
 end
